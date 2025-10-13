e6a5e1aed90c76a564e4c6bbc88dcfbb11098fca
ma3term1nd <ahnaftajwarusa09@gmail.com>
2025-10-12 22:44:36 -0400

    Added AprilTag and ColorSensor

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/main/AprilTag.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/main/AprilTag.java
@@ -2,8 +2,6 @@ package org.firstinspires.ftc.teamcode.main;
 
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
-import org.firstinspires.ftc.robotcore.external.hardware.camera.BuiltinCameraDirection;
-import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import org.firstinspires.ftc.vision.VisionPortal;
@@ -16,76 +14,74 @@ import java.util.List;
 
 public class AprilTag {
 
-    private static boolean USE_WEBCAM = true;  // true for webcam, false for phone camera
     private AprilTagProcessor aprilTag;
     private VisionPortal visionPortal;
 
-    public void init (HardwareMap hwMap, boolean isWebcam) {
-        aprilTag = new AprilTagProcessor //sensor
+    public void init(HardwareMap hwMap) {
+        aprilTag = new AprilTagProcessor // sensor
                 .Builder()
                 .setDrawTagOutline(true)
                 .setOutputUnits(DistanceUnit.INCH, AngleUnit.DEGREES)
                 .setLensIntrinsics(578.272, 578.272, 402.145, 221.506)
                 .build();
 
-        VisionPortal.Builder builder = new VisionPortal.Builder(); //camera
-        builder.addProcessor(aprilTag); //enable the sensor
-        visionPortal = builder.build(); //build the visionPortal
-
-        USE_WEBCAM = isWebcam;
-        if (USE_WEBCAM) {
-            builder.setCamera(hwMap.get(WebcamName.class, "webcam")); //make sure to name it "webcam"
-        } else {
-            builder.setCamera(BuiltinCameraDirection.BACK); //sets it to phone camera
-        }
+        VisionPortal.Builder builder = new VisionPortal.Builder(); // camera
+        builder.addProcessor(aprilTag); // add the sensor
+        visionPortal = builder.build(); // build the VisionPortal
     }
 
-    public boolean isAprilTag () {
-        if (this.aprilTag.getDetections() != null) {
-            return true;
-        } else {
-            return false;
-        }
+    /* METHODS */
+
+    public boolean isAprilTag() {
+        List<AprilTagDetection> detections = aprilTag.getDetections();
+        return detections != null && !detections.isEmpty();
     }
 
     public double getRange() {
-        List<AprilTagDetection> currentDetections = this.aprilTag.getDetections();
-        for (AprilTagDetection detection : currentDetections) {
-            return detection.ftcPose.range;
-        } {
-            return 0;
+        List<AprilTagDetection> currentDetections = aprilTag.getDetections();
+        if (currentDetections != null && !currentDetections.isEmpty()) {
+            return currentDetections.get(0).ftcPose.range;
         }
+        return 0;
     }
 
     public double getAngle() {
-        List<AprilTagDetection> currentDetections = this.aprilTag.getDetections();
-        for (AprilTagDetection detection : currentDetections) {
-            return detection.ftcPose.bearing;
-        } {
-            return 0;
+        List<AprilTagDetection> currentDetections = aprilTag.getDetections();
+        if (currentDetections != null && !currentDetections.isEmpty()) {
+            return currentDetections.get(0).ftcPose.bearing;
         }
+        return 0;
     }
 
     public double getHeight() {
-        List<AprilTagDetection> currentDetections = this.aprilTag.getDetections();
-        for (AprilTagDetection detection : currentDetections) {
-            return detection.ftcPose.elevation;
-        } {
-            return 0;
+        List<AprilTagDetection> currentDetections = aprilTag.getDetections();
+        if (currentDetections != null && !currentDetections.isEmpty()) {
+            return currentDetections.get(0).ftcPose.elevation;
         }
+        return 0;
     }
 
     public double getID() {
-        List<AprilTagDetection> currentDetections = this.aprilTag.getDetections();
-        for (AprilTagDetection detection : currentDetections) {
-            return detection.id;
-        } {
-            return 0;
+        List<AprilTagDetection> currentDetections = aprilTag.getDetections();
+        if (currentDetections != null && !currentDetections.isEmpty()) {
+            return currentDetections.get(0).id;
         }
+        return 0;
     }
 
     public void setOffset(float i) {
         aprilTag.setDecimation(i);
     }
 
+    public void stopWebcam() {
+        if (visionPortal != null) {
+            visionPortal.stopStreaming(); // stops the camera stream
+        }
+    }
+
+    public void startWebcam() {
+        if (visionPortal != null) {
+            visionPortal.resumeStreaming(); // resumes the camera stream
+        }
+    }
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/main/ColorSensor.java
@@ -0,0 +1,99 @@
+package org.firstinspires.ftc.teamcode.main;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.opencv.core.*;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.imgproc.Moments;
+import org.openftc.easyopencv.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/* UTILITY CLASS */
+
+public class ColorSensor extends OpenCvPipeline {
+
+    public enum BallColor { PURPLE, GREEN }
+
+    private OpenCvWebcam webcam;
+    private BallColor targetColor = BallColor.PURPLE; // default color to track
+    private Point ballCenter = null; // stores last detected ball center
+
+    /* INIT CAMERA */
+    public void init(HardwareMap hwMap) {
+        int cameraMonitorViewId = hwMap.appContext
+                .getResources()
+                .getIdentifier("cameraMonitorViewId", "id", hwMap.appContext.getPackageName());
+
+        webcam = OpenCvCameraFactory.getInstance().createWebcam(
+                hwMap.get(WebcamName.class, "webcam"),
+                cameraMonitorViewId);
+
+        webcam.setPipeline(this);
+        webcam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
+            @Override public void onOpened() {}
+            @Override public void onError(int errorCode) {}
+        });
+    }
+
+    @Override
+    public Mat processFrame(Mat input) {
+        Mat hsv = new Mat();
+        Mat mask = new Mat();
+
+        // Convert RGB to HSV
+        Imgproc.cvtColor(input, hsv, Imgproc.COLOR_RGB2HSV);
+
+        // Create mask based on target color
+        if (targetColor == BallColor.PURPLE) {
+            Scalar lowerPurple = new Scalar(125, 50, 50);
+            Scalar upperPurple = new Scalar(155, 255, 255);
+            Core.inRange(hsv, lowerPurple, upperPurple, mask);
+        } else if (targetColor == BallColor.GREEN) {
+            Scalar lowerGreen = new Scalar(35, 50, 50);
+            Scalar upperGreen = new Scalar(85, 255, 255);
+            Core.inRange(hsv, lowerGreen, upperGreen, mask);
+        }
+
+        // Find contours
+        List<MatOfPoint> contours = new ArrayList<>();
+        Imgproc.findContours(mask, contours, new Mat(), Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
+
+        // Reset center before searching
+        ballCenter = null;
+        double maxArea = 0;
+
+        // Find largest contour and its center
+        for (MatOfPoint contour : contours) {
+            double area = Imgproc.contourArea(contour);
+            if (area > 100 && area > maxArea) { // filter out noise, pick largest
+                Moments m = Imgproc.moments(contour);
+                ballCenter = new Point(m.m10 / m.m00, m.m01 / m.m00);
+                maxArea = area;
+            }
+        }
+
+        // Return original image (optional)
+        return input;
+    }
+
+    /* METHODS */
+
+    public void setTargetColor(BallColor color) {
+        targetColor = color;
+    }
+
+    public Point getBallCenter() {
+        return ballCenter;
+    }
+
+    public void stopWebcam() {
+        if (webcam != null) webcam.stopStreaming();
+    }
+
+    public void startWebcam() {
+        if (webcam != null) webcam.startStreaming(320, 240, OpenCvCameraRotation.UPRIGHT);
+    }
+}
