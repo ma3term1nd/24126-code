47b7f7828ef49dcf0162de758968aed217a2d9ee
timmyrawr <timmypham6@gmail.com>
2025-11-19 13:42:41 -0500

    V2

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/TeleOpV2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/TeleOpV2.java
@@ -5,13 +5,14 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.teamcode.OpModes.MecanumClass;
 @TeleOp(name = "teleopV2")
 public class TeleOpV2 extends OpMode {
-    CRServo transer1;
-    DcMotor transfer, shooter;
+    CRServo trasnfertester;
     enum DriveState {
         normalDrive,
         roadRunnerDrive;
@@ -19,14 +20,16 @@ public class TeleOpV2 extends OpMode {
     MecanumClass drive = new MecanumClass();
     public void init(){
 
-        drive.init(hardwareMap);
-
-        shooter = hardwareMap.get(DcMotor.class, "shooter");
-
+        trasnfertester = hardwareMap.get(CRServo.class, "transfer1");
+        trasnfertester.setDirection(DcMotorSimple.Direction.FORWARD);
+        trasnfertester.setPower(0);
 
     }
     public void loop(){ //start of loop
-
+        if(gamepad1.b){
+           trasnfertester.setPower(1);
+        }
+        /*
         DriveState state = DriveState.normalDrive;
         switch(state){
             case normalDrive:
@@ -34,6 +37,7 @@ public class TeleOpV2 extends OpMode {
                 if(gamepad1.a){
                     state = DriveState.roadRunnerDrive; //if press a button, switches to roadrunner mode
                 }
+
 
                 break;
 
@@ -44,5 +48,7 @@ public class TeleOpV2 extends OpMode {
                 }
                 break;
         }
+        */
+
     } //end of loop
 }//end of class
